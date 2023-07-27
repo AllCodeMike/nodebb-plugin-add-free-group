bd3a1787f806ce2cb490bede6701da6758929d07
andresremitter <andresallcode@remitter.com>
2023-07-27 16:19:26 -0500

    new commit person

--- library.js
+++ library.js
@@ -2,6 +2,10 @@
 
 const plugin = {};
 var groups = require.main.require('./src/groups')
+const plugins = require.main.require('./src/plugins');
+const axios = require('axios');
+const controllerHelpers = require.main.require('./src/controllers/helpers');
+				controllerHelpers.redirect(res, `${nconf.get('relative_path')}/${mount}${req.path}`);
 
 plugin.addUserToFreeGroup = async (params) => {
   try {
@@ -15,6 +19,36 @@ plugin.addUserToFreeGroup = async (params) => {
 	groups.join(groupName, params.user.uid, function(err) {
 		console.log("ERROR ON PLUGIN", err);
 	});
+	
+	if(params.data && params.data.registration_plan && params.data.registration_plan !== 'free-plan-membership'){
+		var config = {
+			params:{
+				uid: params.user.uid
+			},
+			validateStatus: function (status) {
+				return status >= 200 && status <= 400
+			}
+		}
+	
+		const response = axios.post('http://127.0.0.1:3000/register-checkout-session/' + params.data.registration_plan, {}, config)
+			.then(response => {
+				// The request was successful, and the response data is available here.
+				window.open(response.data);
+			})
+			.catch(error => {
+				// An error occurred during the request.
+				console.log("error", error);
+		});
+	}
+	
+  } catch (error) {
+    console.log("ERROR ON PLUGIN", error);
+  }
+};
+
+plugin.addUserToFreeGroup = async (params) => {
+  try {
+	
   } catch (error) {
     console.log("ERROR ON PLUGIN", error);
   }
